f8e83ac0e55f7869184cec7e481256c8ab02e1f0
KieuViet <themasterofwind1997@gmail.com>
2022-06-02 20:18:45 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/dx/audiorecorder/MainActivity.java
+++ app/src/main/java/com/example/dx/audiorecorder/MainActivity.java
@@ -78,3 +78,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
